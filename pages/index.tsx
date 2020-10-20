328ae1aac8d0c79ce136d4ee7bc43cdc7a0c9571
Usuario <jcortesg1997@gmail.com>
2020-10-20 16:41:33 +0200

    Configuration with Typescript and Tailwind

---
+++ pages/index.tsx
@@ -0,0 +1,6 @@
+export default function Home(): React.ReactNode {
+  return (
+    <div className="h-screen bg-red-600">
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+    defaultLineHeights: true,
+    standardFontWeights: true,
+  },
+  purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    cursor: ["responsive", "hover", "focus"],
+  },
+  plugins: [],
+};
